f74f00a9dc4b349fab566a89942477a3807ae418
Jaroslaw Kulesza <climbus@gmail.com>
2016-10-08 11:33:44 +0200

    Working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 
 import './App.css';
 import Step from './Step';
 import EditableTitle from './EditableTitle';
 
-import { Navbar, FormControl, FormGroup } from 'react-bootstrap';
+import { Navbar, FormGroup } from 'react-bootstrap';
 
 class App extends Component {
 
@@ -14,17 +13,11 @@ class App extends Component {
         this.state = {
             steps: [{id: 1, label:"Zakładając, że"}, {id: 10, label: "Gdy"}, {id: 100, label:"Wtedy"}],
             focusIndex: 0,
-            title: ''
         }
         this.onArrow = this.onArrow.bind(this);
-        this.onTitleClick = this.onTitleClick.bind(this);
-        this.onEnter = this.onEnter.bind(this);
-        this.onTitleBlur = this.onTitleBlur.bind(this);
         this.addStep = this.addStep.bind(this);
         this.deleteStep = this.deleteStep.bind(this);
         this.renderStep = this.renderStep.bind(this);
-
-        this.altTitle = "Kliknij aby dodać tytuł";
     }
 
     findIndex(id) {
@@ -116,29 +109,7 @@ class App extends Component {
             this.moveCursor(currentId, e.key);
         }
     };
-
-    onTitleClick() {
-       this.setState({title_edit: true}); 
-    };
-
-    onEnter(e) {
-        if (e.key === "Enter") {
-            this.setState({title: e.target.value, title_edit: false}) 
-        }
-    };
-
-    onTitleBlur() {
-        var val = ReactDOM.findDOMNode(this.refs.title).value;
-        this.setState({title: val, title_edit: false}) 
-    }
    
-    getTitle() {
-        if (!this.state.title) {
-            return this.altTitle;
-        }
-        return this.state.title;
-    }
-
     renderStep(step, i) {
         var autoFocus = false;
         
@@ -158,28 +129,18 @@ class App extends Component {
         return this.state.steps.map(this.renderStep)
     }
 
-    renderTitle() {
-        var title_class = ['title',];
-        
-        if (!this.state.title) {
-            title_class.push("grey");
-        }
-
-        if (this.state.title_edit) {
-            return <FormControl defaultValue={ this.state.title } ref="title" className='title' onKeyPress={ this.onEnter } autoFocus onBlur={ this.onTitleBlur } />
-        } else {
-            return <h2 className={ title_class.join(" ") } title={ this.altTitle } onClick={ this.onTitleClick }>{ this.getTitle() }</h2>
-        }
-    }
-
     render() {
        return (
              <div className="App">
                 <Navbar>
-                    <h1>Scenario creator</h1>
-                    <FormGroup>
-                        <EditableTitle />
-                    </FormGroup>
+                    <Navbar.Header>
+                    </Navbar.Header>
+                    <Navbar.Collapse>
+                        <h1>Scenario creator</h1>
+                        <FormGroup>
+                            <EditableTitle />
+                        </FormGroup>
+                    </Navbar.Collapse>
                 </Navbar>
 
                 { this.renderSteps() }
